d11dc4d73cf509c8bea7219e396b8342168ece2a
farmermichael1 <farmer_michael1@columbusstate.edu>
2022-11-22 02:46:58 +0000

    Completed GO assignment

--- src/welcome-app/main.go
+++ src/welcome-app/main.go
@@ -5,6 +5,7 @@ import (
 	"html/template"
 	"net/http"
 	"time"
+	"encoding/json"
 )
 
 // Create a struct that holds information to be displayed in our HTML file
@@ -12,6 +13,46 @@ type Welcome struct {
 	Name string
 	Time string
 }
+
+
+type JsonResponse struct {
+	Value1 string `json:"key1"`
+	Value2 string `json:"key2"`
+	JsonNested JsonNested `json:"jsonNested"`
+}
+
+type JsonNested struct {
+	NestedValue1 string `json:"nestedkey1"`
+	NestedValue2 string `json:"nestedkey2"`
+}
+
+
+
+
+
+//Name struct
+type JsonName struct {
+	Fname string `json:"first name"`
+	Lname string `json:"last name"`
+	Address Address `json:"Address"`
+	Contact Contact `json:"Contact Info"`
+}
+
+type Address struct {
+	Street string `json:"street"`
+	City string `json:"city"`
+	Zipcode string `json:"zipcode"`
+	State string `json:"state"`
+}
+
+type Contact struct {
+	Email string `json:"email"`
+	Phone string `json:"phone"`
+}
+
+
+
+
 
 // Go application entrypoint
 func main() {
@@ -23,6 +64,42 @@ func main() {
 	// the relative path). We wrap it in a call to template.Must() which handles any errors and halts if there are fatal errors
 
 	templates := template.Must(template.ParseFiles("templates/welcome-template.html"))
+
+	nested := JsonNested{
+		NestedValue1: "first nested value",
+		NestedValue2: "second nested value",
+	}
+
+	jsonResp := JsonResponse{
+		Value1: "some Data",
+		Value2: "other Data",
+		JsonNested: nested,
+	}
+
+	//contact data
+	contact := Contact{
+		Email: "thisisaemail1@yahoo.com",
+		Phone: "123-456-7890",
+	}
+
+	//address data
+	jsonAddress := Address {
+		Street: "1234 Washington Drive",
+		City: "Babylon",
+		Zipcode: "55798",
+		State: "Alaska",
+	}
+	
+	//name data
+	jsonNameData := JsonName{
+		Fname: "Frank",
+		Lname: "Ford",
+		Address: jsonAddress,
+		Contact: contact,
+	}
+
+
+	
 
 	//Our HTML comes with CSS that go needs to provide when we run the app. Here we tell go to create
 	// a handle that looks in the static directory, go then uses the "/static/" as a url that our
@@ -49,6 +126,16 @@ func main() {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
+
+	http.HandleFunc("/jsonResponse", func(w http.ResponseWriter, r *http.Request){
+		json.NewEncoder(w).Encode(jsonResp)
+	})
+
+	http.HandleFunc("/jsonName", func(w http.ResponseWriter, r *http.Request){
+		json.NewEncoder(w).Encode(jsonNameData)
+	})
+	
+	// third path, get/fetch, return a json object like an API 2 nested objects {firstname:"", lastname:"", address:{street:"", city...}, contactInfo:{email:"", phone:""}}
 
 	//Start the web server, set the port to listen to 8080. Without a path it assumes localhost
 	//Print any errors from starting the webserver using fmt
